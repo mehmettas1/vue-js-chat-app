d435865ac080c12e14d51c5527b067b690053553
mehmet <tas349873@gmail.com>
2023-09-25 15:51:57 +0300

    added

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="">sss</div>
+</template>
+
+<script>
+export default {};
+</script>
+<style></style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>Welcome</div>
+  <div class="welcome container">Welcome</div>
 </template>
 
 <script>
 export default {};
 </script>
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>

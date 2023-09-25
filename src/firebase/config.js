0169d7f8c2720de6f1b23d1ee04751351e327ef7
mehmet <tas349873@gmail.com>
2023-09-25 16:09:28 +0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCisBQmyiAt2FRgLMDa37suWyztFtY0_DA",
+  authDomain: "blog-vue3-new.firebaseapp.com",
+  projectId: "blog-vue3-new",
+  storageBucket: "blog-vue3-new.appspot.com",
+  messagingSenderId: "961116365060",
+  appId: "1:961116365060:web:03cb3e4e794c358559b051",
+};

e8088baa9807a148e1b949097005c4a3e1d1322f
mehmet <tas349873@gmail.com>
2023-09-29 23:16:42 +0300

    add

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -4,10 +4,12 @@
     <div v-if="showLogin">
       <h2>Login</h2>
       <LoginForm />
+      <p>No account yet? <span @click="showLogin =false" > Signup</span></p>
     </div>
     <div v-else>
 <h2>Signup</h2>
       <SignUpForm />
+      <p>Already registered? <span @click="showLogin =true" > Login</span></p>
     </div>
   </div>
 </template>

69b4aa50e6310499892ff1d11bb30e834ffaf62d
mehmet <tas349873@gmail.com>
2023-09-29 23:41:03 +0300

    signup form

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -19,13 +19,15 @@
 
 <script>
 import { ref } from "vue";
+import useSignup from "../composables/useSignup";
 export default {
   setup() {
+    const { error, signup } = useSignup;
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
-    const handleSubmit = () => {
-      console.log(displayName.value, email.value, password.value);
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
     };
     return { displayName, email, password, handleSubmit };
   },
---
+++ src/composables/useSignup.js
@@ -0,0 +1,25 @@
+import {ref} from "vue";
+import {projectAuth} from "../firebase/config";
+
+
+const error = ref(null)
+
+const signup = async (email,password,displayName)=>{
+error.value = null
+try {
+    const res = await projectAuth.createUserWithEmailAndPassword(email,password)
+    if(!response){
+        throw new Error('Could not complete signup!')
+    }
+    console.log(res.user);
+} catch (err) {
+    console.log(err.message);
+    error.value = err.message
+}
+}
+
+const useSignup = ()=>{
+    return{error,signup}
+}
+
+export default useSignup;

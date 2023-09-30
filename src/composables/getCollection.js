c792ff721d30053bc105ecbd32d65a7138c63fd8
mehmet <tas349873@gmail.com>
2023-09-30 20:40:19 +0300

    add

---
+++ src/composables/getCollection.js
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+import { projectFirestore } from "../firebase/config";
+
+const getCollection = (collection) => {
+  const error = ref(null);
+  const documents = ref(null);
+  let collectionRef = projectFirestore
+    .collection(collection)
+    .orderBy("createdAt");
+    collectionRef.onSnapshot((snap)=> {
+        let result = []
+        snap.docs.forEach(doc=>{
+            doc.data().createdAt && result.push({...doc.data(),id:doc.id})
+        })
+       documents.value=result
+        error.value=null
+    },(err)=>{
+        console.log(err.message);
+        documents.value = null
+        error.value='could not fetch data'
+    })
+
+  return { error, documents };
+};
+
+export default getCollection;

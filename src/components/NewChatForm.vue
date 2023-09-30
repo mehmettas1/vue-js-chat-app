b820f4a8ce009bfc8ae3cb668978f04cd9c0e45d
mehmet <tas349873@gmail.com>
2023-09-30 20:02:05 +0300

    style

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -31,4 +31,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+form{
+  margin: 10px;
+} 
+
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0 ;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+  
+
+}</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -25,4 +25,12 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+
+
+
+
+
+
+
+</style>

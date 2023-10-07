e8f0936d8a711bd9e5e93010147e7735f80e687d
mehmet <tas349873@gmail.com>
2023-10-07 21:47:12 +0300

    add

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>ss</div>
+  <div class="chat-window">
+    <div class="error" v-if="error">{{ error }}</div>
+
+    <div v-if="documents" class="messages">
+      <div v-for="doc in documents" :key="doc.id" class="single">
+    <span class="created-at">
+        {{ doc.createdAt }}
+    </span>
+    <span class="name">
+        {{ doc.name }}
+    </span>
+    <span class="message">
+        {{ doc.message }}
+    </span>
+</div>
+    </div>
+  </div>
 </template>
 <script>
-import getCollection from '@/composables/getCollection';
+import getCollection from "@/composables/getCollection";
 
-
-getCollection
 export default {
-setup(){
-    const {error,documents} =
-}
-}
+  setup() {
+    const { error, documents } = getCollection("messages");
+    return { error, documents };
+  },
+};
 </script>
